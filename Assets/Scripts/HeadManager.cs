05f9d0cbc91bf48dfb12fbbf1377b1021cb234de
Ludovic Moge <ludovic.moge@gmail.com>
2019-01-27 00:08:02 +0100

    wip spacesuit helmet

---
+++ Assets/Scripts/HeadManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadManager : MonoBehaviour {
+
+    private Rigidbody rb;
+    public GameObject player;
+
+    [Header("Helmet movement")]
+    public GameObject helmetTarget;
+
+    public float rotationSpeed;
+
+    public float clampDistance;
+    public float easeinDistance;
+    public float easeinMinSpeed;
+    public float easeoutDistance;
+    public float easeoutMinSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //rb = player.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = helmetTarget.transform.position;
+
+        Quaternion diff = transform.rotation * Quaternion.Inverse(helmetTarget.transform.rotation);
+        float diffAngle = Quaternion.Angle(transform.rotation, helmetTarget.transform.rotation);
+
+
+        float currentRotationSpeed = 0;
+
+        // clamp
+        if (diffAngle > clampDistance)
+        {
+            // mettre casque à distance clampée
+        }
+
+        // ease in
+        else if (diffAngle <= easeinDistance)
+        {
+            currentRotationSpeed = Mathf.Lerp(easeinMinSpeed, rotationSpeed, (diffAngle - easeinDistance)/(clampDistance - easeinDistance)); // 0 -> 1
+        }
+
+        //normal rot
+        else if (diffAngle < easeoutDistance)
+        {
+            currentRotationSpeed = rotationSpeed;
+        }
+
+        // ease out
+        else if (diffAngle >= easeoutDistance)
+        {
+            currentRotationSpeed = Mathf.Lerp(easeoutDistance, rotationSpeed, (diffAngle - 0) / (easeoutDistance - 0)); // 1 -> 0
+        }
+
+        else
+        {
+            Debug.LogError("Unexpected case in helmet rotation !!");
+        }
+
+        Quaternion.RotateTowards(transform.rotation, helmetTarget.transform.rotation, currentRotationSpeed);
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //print("player collision");
+        //rb.velocity = Vector3.zero;
+    }
+}
